1e82b95f834ec030699bea8a910f35bcd275972e
Jason McBride <jmcbride.isye@gmail.com>
2019-07-23 03:18:41 +0000

    About page details added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'quotes#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :quotes
+
+  # JASON - i added this instruction here to create a new  "about" page.
+  get 'about', to: 'quotes#about'
 end
